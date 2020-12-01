9912385f95cd0bf4e86e1dd345f1387c87116820
abhishek_jn <63057914+abhishek-netizen@users.noreply.github.com>
2020-12-01 23:24:39 +0530

    Add files via upload

---
+++ creating my first API/app.js
@@ -0,0 +1,124 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlsParser:true});
+
+//Lets create a SCHEMA
+const articleSchema = {
+  title:String,
+  content:String
+};
+
+//////////////////////// Request Targetting all of the articles //////////////////////////
+
+app.route("/articles")
+.get(function(req,res){
+  Article.find(function(err, foundArticles){
+    if(!err){
+      res.send(foundArticles);
+    }else{
+      res.send(err);
+    }
+  });
+})
+.post(function(req,res){
+  console.log(req.body.title);
+  console.log(req.body.content);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+  newArticle.save(function(err){
+    if(!err){
+      res.send("succesfully added a new article!")
+    }else{
+      res.send(err);
+    }
+  });
+})
+.delete(function(req,res){
+  Article.deleteMany(function(err){
+    if(!err){
+      res.send("succesfully deleted  all the articles!!");
+    }else{
+      res.send(err);
+    }
+  });
+});
+
+
+
+const Article = mongoose.model("Article",articleSchema);
+
+//////////////////////// Request Targetting a Specific article //////////////////////////
+app.route("/articles/:articleTitle")
+
+.get(function(req,res){
+
+  Article.findOne({title:req.params.articleTitle},function(err,foundArticle){
+    if (foundArticle){
+      res.send(foundArticle);
+    }else{
+      res.send("No articles matching that title was found !!")
+    }
+  });
+})
+.put(function(req,res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},{overwrite:true},
+    function(err){
+      if(!err){
+        res.send("successfully updated the article")
+      }
+    }
+  );
+})
+.patch(function(req,res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("successfully updated the article");
+      }else{
+        res.send(err);
+      }
+    }
+  );
+})
+.delete(function(req,res){
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if(!err){
+        res.send("Successfully deleted")
+      }else{
+        res.send(err);
+      }
+    }
+  );
+});
+
+
+
+
+//TODO
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
