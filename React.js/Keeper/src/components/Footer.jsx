0553a7f1afe31142dba6beb0a573d8ce0b509e19
abhishek_jn <63057914+abhishek-netizen@users.noreply.github.com>
2020-12-04 00:50:34 +0530

    Add files via upload

---
+++ React.js/Keeper/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const years = new Date().getFullYear();
+  return (
+    <footer>
+      <p> copyright © {years}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ React.js/Keeper/src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Note(){
+  return (
+    <div className="note">
+      <h1> Do you know what is DBSCAN stands for? </h1>
+    <p>
+      Density Based Spacial Clustering Application with Noise.
+      </p>
+    </div>
+  )
+}
+
+
+export default Note;
