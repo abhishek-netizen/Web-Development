7832e6f52858681e595c956b5acffab8b4239337
abhishek_jn <63057914+abhishek-netizen@users.noreply.github.com>
2020-12-18 11:32:26 +0530

    Add files via upload

---
+++ React.js/Creating a todolist using spread operator concepts/src/components/App.jsx
@@ -0,0 +1,43 @@
+import React,{useState} from "react";
+
+function App() {
+
+const[inputText,setInputText] = useState("");
+const[items,setItems] = useState([]);
+
+function handleChange(event){
+const newValue = event.target.value;
+setInputText(newValue);
+console.log(newValue)
+}
+
+function handleClick(){
+setItems((prevItems) => {
+  return [...prevItems,inputText]
+})
+setInputText("")
+}
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText}/>
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem)=>{
+            return <li>{todoItem}</li>
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
