195798f7531d100fbfae212071ece22c8d47a593
abhishek_jn <63057914+abhishek-netizen@users.noreply.github.com>
2020-12-18 17:49:55 +0530

    Add files via upload

---
+++ React.js/Keeper app last and final/src/components/CreateArea.jsx
@@ -0,0 +1,38 @@
+import React,{useState} from "react";
+
+function CreateArea(props) {
+ const[note,setNote] = useState({
+   title:"",
+   content:""
+ });
+
+ function handleChange(event){
+ const { name,value } = event.target;
+
+ setNote(prevNote => {
+   return {
+     ...prevNote,
+     [name]:value
+  };
+ });
+ }
+
+function submitNote(event){
+  props.onAdd(note);
+  setNote({title: " ",content: " "});
+event.preventDefault();
+}
+
+
+  return (
+    <div>
+      <form>
+        <input name="title" onChange={handleChange} values={note.title}   placeholder="Title" />
+        <textarea name="content" onChange={handleChange}  values={note.content}  placeholder="Take a note..." rows="3" />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
