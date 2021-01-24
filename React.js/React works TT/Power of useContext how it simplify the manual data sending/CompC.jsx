08bb894be04c93d7c58aabfef7506c094bd5977f
abhishek_jn <63057914+abhishek-netizen@users.noreply.github.com>
2021-01-24 20:57:42 +0530

    Add files via upload

---
+++ React.js/React works TT/Power of useContext how it simplify the manual data sending/CompC.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {FirstName,LastName} from './App';
+
+const CompC = ()=>{ 
+  return(
+    <>
+  <FirstName.Consumer> 
+    {(fname)=>{
+       return(
+         <>
+         <LastName.Consumer>
+            {(lname)=>{
+              return(
+                <>
+                <h1>Hey i am getting values as : {fname} {lname}, from the App component</h1>
+                </>
+              )
+            }}
+         </LastName.Consumer>
+         </>
+       ) 
+     }}
+    
+  </FirstName.Consumer>
+
+
+
+    
+    </>
+  )
+}
+export default CompC;
