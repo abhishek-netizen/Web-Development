c38615d11e8d1cc6a0a753d18275a26607d6300e
abhishek_jn <63057914+abhishek-netizen@users.noreply.github.com>
2021-01-27 22:19:02 +0530

    Add files via upload

---
+++ Node Js/6 path  modules in node/index.js
@@ -0,0 +1,8 @@
+//Path module in nodeJS
+const path = require('path');
+
+console.log(path.dirname('C:/Users/hp/Desktop/node works TT/index.js'));
+console.log(path.extname('C:/Users/hp/Desktop/node works TT/index.js'));
+console.log(path.basename('C:/Users/hp/Desktop/node works TT/index.js'));
+console.log(path.parse('C:/Users/hp/Desktop/node works TT/index.js')); // i think this only is enough
+
